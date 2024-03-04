2c363e1fc7c73b2187cd1fc62112e6f5889ad189
Kevin Dang <kevinthedang_1@outlook.com>
2024-03-04 11:40:54 -0800

    reformat messageStyle.ts

--- src/commands/messageStyle.ts
+++ src/commands/messageStyle.ts
@@ -5,32 +5,34 @@ import { SlashCommand } from '../utils/commands.js'
 export var embedStyle = true
 
 export const MessageStyle: SlashCommand = {
-  name: 'message-style',
-  description: 'sets the message style to embed or normal',
-  
-  // set available user options to pass to the command
-  options: [
-    {
-      name: 'embed',
-      description: 'toggle embedded or normal message',
-      type: ApplicationCommandOptionType.Boolean,
-      required: true
-    }
-  ],
+    name: 'message-style',
+    description: 'sets the message style to embed or normal',
 
-  // Query for message information and set the style
-  run: async (client: Client, interaction: CommandInteraction) => {
-    // fetch channel and message
-    const channel = await client.channels.fetch(interaction.channelId)
-    if (!channel || channel.type !== ChannelType.GuildText) return
-    
-    // set the message style
-    if (interaction.options.get('embed')?.value) {
-      embedStyle = true
-      interaction.reply('Message style set to embed.')
-    } else {
-      embedStyle = false
-      interaction.reply('Message style set to normal.')
+    // set available user options to pass to the command
+    options: [
+        {
+            name: 'embed',
+            description: 'toggle embedded or normal message',
+            type: ApplicationCommandOptionType.Boolean,
+            required: true
+        }
+    ],
+
+    // Query for message information and set the style
+    run: async (client: Client, interaction: CommandInteraction) => {
+        // fetch channel and message
+        const channel = await client.channels.fetch(interaction.channelId)
+        if (!channel || channel.type !== ChannelType.GuildText) return
+
+        // set the message style
+        if (interaction.options.get('embed')?.value)
+            embedStyle = true
+        else
+            embedStyle = false
+
+        interaction.reply({
+            content: `Message style preferences for embed set to: \`${embedStyle}\``,
+            ephemeral: true
+        })
     }
-  }
 }
