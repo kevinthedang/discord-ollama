04846863323bdd218aeff3b0c6f942495a0ab376
Kevin Dang <kevinthedang_1@outlook.com>
2024-03-31 00:13:26 -0700

    add: disable chat command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,9 +2,11 @@ import { SlashCommand } from '../utils/commands.js'
 import { ThreadCreate } from './threadCreate.js'
 import { MessageStyle } from './messageStyle.js'
 import { MessageStream } from './messageStream.js'
+import { Disable } from './disable.js'
 
 export default [
     ThreadCreate,
     MessageStyle,
-    MessageStream
+    MessageStream,
+    Disable
 ] as SlashCommand[]
