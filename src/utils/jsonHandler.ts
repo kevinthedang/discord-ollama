719be996455f9342ce190a80c6c6421efc043c85
kevinthedang <kevinthedang_1@outlook.com>
2024-06-20 23:54:18 -0700

    Add: Channel History handler by user

--- src/utils/jsonHandler.ts
+++ src/utils/jsonHandler.ts
@@ -1,4 +1,4 @@
-import { ThreadChannel } from 'discord.js'
+import { TextChannel, ThreadChannel } from 'discord.js'
 import { UserMessage } from './events.js'
 import fs from 'fs'
 import path from 'path'
@@ -17,6 +17,13 @@ export interface Configuration {
 export interface Thread {
     readonly id: string
     readonly name: string
+    messages: UserMessage[]
+}
+
+export interface Channel {
+    readonly id: string
+    readonly name: string
+    readonly user: string
     messages: UserMessage[]
 }
 
@@ -86,7 +93,7 @@ export function openThreadInfo(filename: string, thread: ThreadChannel, messages
     if (fs.existsSync(fullFileName)) {
         fs.readFile(fullFileName, 'utf8', (error, data) => {
             if (error)
-                console.log(`[Error: openConfig] Incorrect file format`)
+                console.log(`[Error: openThreadInfo] Incorrect file format`)
             else {
                 const object = JSON.parse(data)
                 object['messages'] = messages as []
@@ -114,6 +121,76 @@ export function openThreadInfo(filename: string, thread: ThreadChannel, messages
  */
 export async function getThread(filename: string, callback: (config: Thread | undefined) => void): Promise<void> {
     // attempt to read the file and get the configuration
+    const fullFileName = `data/${filename}`
+    if (fs.existsSync(fullFileName)) {
+        fs.readFile(fullFileName, 'utf8', (error, data) => {
+            if (error) {
+                callback(undefined) 
+                return // something went wrong... stop
+            }
+            callback(JSON.parse(data))
+        })
+    } else {
+        callback(undefined) // file not found
+    }
+}
+
+/**
+ * Method to open the channel history
+ * 
+ * @param filename name of the json file for the channel by user
+ * @param channel the text channel info
+ * @param user the user's name
+ * @param messages their messages
+ */
+export async function openChannelInfo(filename: string, channel: TextChannel, user: string, messages: UserMessage[] = []): Promise<void> {
+    // thread exist handler
+    const isThread: boolean = await new Promise((resolve) => {
+        getThread(`${channel.id}.json`, (threadInfo) => {
+            if (threadInfo?.messages)
+                resolve(true)
+            else
+                resolve(false)
+        })
+    })
+
+    // This is an existing thread, don't create another json
+    if (isThread) return
+
+    const fullFileName = `data/${filename}-${user}.json`
+    if (fs.existsSync(fullFileName)) {
+        fs.readFile(fullFileName, 'utf8', (error, data) => {
+            if (error)
+                console.log(`[Error: openChannelInfo] Incorrect file format`)
+            else {
+                const object = JSON.parse(data)
+                if (object['messages'].length === 0)
+                    object['messages'] = messages as []
+                else if (object['messages'].length !== 0 && messages.length !== 0)
+                    object['messages'] = messages as []
+                fs.writeFileSync(fullFileName, JSON.stringify(object, null, 2))
+            }
+        })
+    } else { // file doesn't exist, create it
+        const object: Configuration = JSON.parse(`{ \"id\": \"${channel?.id}\", \"name\": \"${channel?.name}\", \"user\": \"${user}\", \"messages\": []}`)
+
+        const directory = path.dirname(fullFileName)
+        if (!fs.existsSync(directory))
+            fs.mkdirSync(directory, { recursive: true })
+
+        // only creating it, no need to add anything
+        fs.writeFileSync(fullFileName, JSON.stringify(object, null, 2))
+        console.log(`[Util: openChannelInfo] Created '${fullFileName}' in working directory`)
+    }
+}
+
+/**
+ * Method to get the channel information/history
+ * 
+ * @param filename name of the json file for the channel by user
+ * @param callback function to handle resolving message history
+ */
+export async function getChannelInfo(filename: string, callback: (config: Channel | undefined) => void): Promise<void> {
     const fullFileName = `data/${filename}`
     if (fs.existsSync(fullFileName)) {
         fs.readFile(fullFileName, 'utf8', (error, data) => {
