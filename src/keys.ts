af23db20bb452dd0411a044c3810ff791e9d4dd7
Kevin Dang <77701718+kevinthedang@users.noreply.github.com>
2024-08-03 09:48:47 -0700

    Removed GUILD_ID as an Environment Variable (#103)

--- src/keys.ts
+++ src/keys.ts
@@ -4,7 +4,6 @@ export const Keys = {
     clientToken: getEnvVar('CLIENT_TOKEN'),
     model: getEnvVar('MODEL'),
     clientUid: getEnvVar('CLIENT_UID'),
-    guildId: getEnvVar('GUILD_ID'),
     ipAddress: getEnvVar('OLLAMA_IP'),
     portAddress: getEnvVar('OLLAMA_PORT'),
 } as const // readonly keys
