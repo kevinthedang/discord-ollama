d9f9100761135733797c60d11bc8d34371a8b8aa
Jonathan Smoley <jtsmoley@icloud.com>
2024-09-30 12:38:38 -0700

    Add: skeleton suite for command tests (#119)

--- tests/commands.test.ts
+++ tests/commands.test.ts
@@ -12,7 +12,7 @@ import commands from '../src/commands'
  * @param name name of the test suite
  * @param fn function holding tests to run
  */
-describe('#commands', () => {
+describe('Commands Existence', () => {
     // test definition of commands object
     it('references defined object', () => {
         // toBe compares the value to the expected value
@@ -23,5 +23,50 @@ describe('#commands', () => {
     it('references specific commands', () => {
         const commandsString = commands.map(e => e.name).join(', ')
         expect(commandsString).toBe('thread, private-thread, message-style, message-stream, toggle-chat, shutoff, modify-capacity, clear-user-channel-history')
+    })
+})
+
+/**
+ * User Commands Test suite for testing out commands
+ * that would be run by users when using the application.
+ */
+describe('User Command Tests', () => {
+    // test capacity command
+    it('run modify-capacity command', () => {
+
+    })
+
+    it('run clear-user-channel-history command', () => {
+        
+    })
+
+    it('run message-stream command', () => {
+        
+    })
+
+    it('run message-style command', () => {
+        
+    })
+    
+    it('run thread command', () => {
+        
+    })
+
+    it('run private-thread command', () => {
+        
+    })
+})
+
+/**
+ * Admin Commands Test suite for running administrative 
+ * commands with the application.
+ */
+describe('Admin Command Tests', () => {
+    it('run shutoff command', () => {
+        
+    })
+
+    it('run toggle-chat command', () => {
+        
     })
 })
