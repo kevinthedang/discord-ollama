c5a58da1b60ec827a2b54239105778a29c09eef9
Kevin Dang <kevinthedang_1@outlook.com>
2024-10-05 18:38:55 -0700

    Add: IPv4 Address Validation

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,6 +3,7 @@ import { config } from 'dotenv'
 
 // resolve config file
 const envFilePath = resolve(process.cwd(), '.env')
+const ipValidate: RegExp = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/
 
 // set current environment variable file
 config({ path: envFilePath })
@@ -23,6 +24,11 @@ export function getEnvVar(name: string, fallback?: string): string {
     if (name === "CLIENT_TOKEN")
         if (value.length < 72) throw new Error(`The "CLIENT_TOKEN" provided is not of at least length 72. 
             This is probably an invalid token unless Discord updated their token policy. Please provide a valid token.`)
+
+    // validate IPv4 address found in environment variables
+    if (name.endsWith("_IP") || name.endsWith("_ADDRESS"))
+        if (!ipValidate.test(value))
+            throw new Error(`Environment variable ${name} does not follow IPv4 formatting.`)
 
     // return env variable
     return value
