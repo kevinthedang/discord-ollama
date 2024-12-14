fe1f7ce5ec9096763811e9f1de0a0d8e6393f6f0
Kevin Dang <77701718+kevinthedang@users.noreply.github.com>
2024-12-13 16:55:57 -0800

    Remove Message Style Command (#149)
    * Remove: Message Style Command
    
    * Update: version increment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,6 @@
 // Centralized import index
 export * from './env.js'
 export * from './events.js'
-export * from './messageEmbed.js'
 export * from './messageNormal.js'
 export * from './commands.js'
 export * from './configInterfaces.js'
