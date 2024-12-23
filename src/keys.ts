400e52c349cf4d22d0aedee90acbde6491b0b6d3
JT2M0L3Y <jtsmoley@icloud.com>
2024-12-23 12:18:53 -0800

    Fixed: redis ip name in keys.ts

--- src/keys.ts
+++ src/keys.ts
@@ -5,7 +5,7 @@ export const Keys = {
     ipAddress: getEnvVar('OLLAMA_IP', '127.0.0.1'), // default ollama ip if none
     portAddress: getEnvVar('OLLAMA_PORT', '11434'), // default ollama port if none
     defaultModel: getEnvVar('MODEL', 'llama3.2'),
-    redisHost: getEnvVar('REDIS_HOST', '172.18.0.4'), // default redis host if none
+    redisHost: getEnvVar('REDIS_IP', '172.18.0.4'), // default redis host if none
     redisPort: parseInt(getEnvVar('REDIS_PORT', '6379')) // default redis port if none
 } as const // readonly keys
 
